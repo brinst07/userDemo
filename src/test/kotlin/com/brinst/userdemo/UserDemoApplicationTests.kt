149849d563ea68ec9b8590b1051a76a939f70465
brinst07 <brinst07@gmail.com>
2023-04-17 00:01:53 +0900

    first commit

---
+++ src/test/kotlin/com/brinst/userdemo/UserDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brinst.userdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
