3f026642609a39c6772cee9bb4de7ed9b812feeb
brinst07 <brinst07@gmail.com>
2023-06-16 22:08:45 +0900

    user 생성 test code 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,11 @@ dependencies {
     testImplementation ("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation ("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation ("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    runtimeOnly("com.h2database:h2")
 }
 
 
--- src/test/kotlin/com/brinst/userdemo/service/UserServiceTests.kt
+++ src/test/kotlin/com/brinst/userdemo/service/UserServiceTests.kt
@@ -1,16 +1,33 @@
 package com.brinst.userdemo.service
 
+import com.brinst.userdemo.config.JwtService
+import com.brinst.userdemo.dto.user.UserRequestDTO
 import com.brinst.userdemo.service.user.UserService
 import io.kotest.core.spec.style.DescribeSpec
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
-@WebMvcTest(UserService::class)
-class UserServiceTests : DescribeSpec({
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceTests(
+        private val userService: UserService,
+        private val jwtService: JwtService
+) : DescribeSpec({
 
     describe("user 생성시") {
         context("올바른 데이터가 전달되면") {
-
-            it ("생성된 user 정보가 리턴된다.")
+            //올바른 데이터
+            val userRequestDTO = UserRequestDTO(
+                    username = "brinst",
+                    email = "brinst@test.com",
+                    password = "q1w2e3r4!"
+            )
+            it("생성된 user 정보가 리턴된다.") {
+                val registerUserToken = userService.registerUser(userRequestDTO)
+                val extractUsername = jwtService.extractUsername(registerUserToken)
+                extractUsername shouldBe userRequestDTO.username
+            }
         }
     }
 })
